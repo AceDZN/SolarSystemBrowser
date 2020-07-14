f0af9e167303f9e465cb8999c936ed12e8c5a6b4
Alex Sinalovsky <design@acedzn.com>
2020-07-14 23:34:40 +0300

    first commit

---
+++ precache-manifest.19ba63816f504da581e03f20acf97170.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e4f46eb45c23811d380f4e23f92f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d46fc2c4d79bdd87c9f9",
+    "url": "/static/css/main.e2be817b.chunk.css"
+  },
+  {
+    "revision": "517d3877b416cdff6c46",
+    "url": "/static/js/2.e9d45a99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e9d45a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46fc2c4d79bdd87c9f9",
+    "url": "/static/js/main.bad4d73b.chunk.js"
+  },
+  {
+    "revision": "821d8798fad7ff4bcec8",
+    "url": "/static/js/runtime-main.20f04ee9.js"
+  },
+  {
+    "revision": "fe2100226016c7273b36a7ada271889b",
+    "url": "/static/media/close.fe210022.svg"
+  },
+  {
+    "revision": "6a40403a6d71f16f05529ebcc3975463",
+    "url": "/static/media/stars.6a40403a.svg"
+  }
+]);
